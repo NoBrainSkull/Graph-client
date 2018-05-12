1132402c905c89f30e8f63b9cf8e847c355d6c1e
cprice <cprice@tutanota.com>
2018-05-12 18:30:54 +0200

    [build] basic configuration

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Graph-client
 Discover services DNS and initialize graphQl Client accordingly
+
+:construction::construction::construction::construction::construction::construction::construction::construction::construction::construction:
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import { GraphQLClient } from 'graphql-request'
+
+const defaultHeaders = {
+  'x-api-key': getApiKey()
+}
+
+export default function(serviceName, headers) {
+  return new GraphQLClient(resolveEndpoint(serviceName), {
+    headers: {
+      ...defaultHeaders,
+      ...headers
+    }
+  })
+}
+
+const resolveEndpoint = serviceName => {
+  //TODO Use DNS resolver to set appropriate endpoint.
+  return 'https://SERVICE.aws.yea'
+}
+
+const getApiKey = () => {
+  //TODO Use appropriate service to get api-key
+  return 'APIKEYVALUE'
+}
